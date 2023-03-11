26ecadbbc819b31db2eb424ce57f32fea461ab64
Tetre101 <basilpeter989@gmail.com>
2023-03-11 01:39:21 +0100

    new todo event updated

--- main.js
+++ main.js
@@ -14,3 +14,39 @@ form.addEventListener("submit", (event) => {
   addTodoItem(input.value);
   input.value = ""; // clear the input field after adding a todo item
 });
+function addTodoItem(todoText) {
+  const todo = {
+    id: Date.now(), // create a unique id using the current date and time
+    text: todoText,
+    created_at: new Date(), // store the date and time the todo was created
+  };
+  todos.push(todo); // add the todo item to the todos array
+  displayTodoItems(todos); // display the updated todos list
+}
+
+// Function to display the todo items
+function displayTodoItems(todosToDisplay) {
+  // Clear the current list of todos
+  list.innerHTML = "";
+
+  // Iterate over the todos array and create an HTML list item for each todo item
+  todosToDisplay.forEach((todo) => {
+    const li = document.createElement("li");
+    const date = new Date(todo.created_at);
+    const formattedDate = date.toLocaleString("en-US", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    });
+    li.innerHTML = `
+        <span>${todo.text}</span>
+        <span class="created">${formattedDate}</span>
+        <button class="delete" data-id="${todo.id}">Delete</button>
+        <button class="edit" data-id="${todo.id}">Edit</button>
+      `;
+    list.appendChild(li);
+  });
+}
